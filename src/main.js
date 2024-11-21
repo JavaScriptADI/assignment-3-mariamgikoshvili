b8213ec6b98b78803722b7950f7819352d331722
mariamgikoshvili <158741806+mariamgikoshvili@users.noreply.github.com>
2024-11-21 16:50:45 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 // write your code in this directory
 
-
 // davaleba 1
 
 for(let i = 1; i <= 10; i++){
@@ -31,18 +30,26 @@ for (let i = 0; i < 10; i += 2) {
 // What does the break statement do? Explain and give an example.
 
 // შეწყვეტს ციკლს
+
 for (let i = 0; i < 10; i += 2) {
-    if(i==6) break
+    if(i === 6){
+        break;
+    }
     console.log(i);  
 }
+   //გამოიტანს: 1,2,3,4,5
 
 // davaleba 6
 // What does the break statement do? Explain and give an example.
 
 // შეწყვეტს ციკლს
+
 for (let i = 0; i < 10; i += 2) {
-    if(i==6) continue
+    if(i===6){
+        continue;
+    }
     console.log(i);  
+    //გამოიტანს: 1,2,3,4,5,7,8,9
 }
 
 // აქ გამოტოვებს 6 ს
@@ -52,15 +59,17 @@ for (let i = 0; i < 10; i += 2) {
 let i ;
 
 while (true){
-    i = Math.random() * 100
-    console.log(i)
-    if (i == 27) break
+    i = Math.random() * 100 + 1
+    console.log(i);
+    if (i === 27) {
+        break;
+    }
 }
 
 // davaleba 8
 
 for(let i = 1; i<=100; i++){
-    if(i%15==0){
+    if(i % 3 === 0 && i % 5 === 0){
         console.log('FizzBuzz')
     }
     else if(i%3==0){
@@ -86,3 +95,34 @@ for(let i = 0; i<100; i++){
 
 // write a program that prints the sum of all even numbers between 1 and 1000.
 
+let sumnumbers = 0;
+for (let i = 1; i <= 1000; i++) { 
+    if(i%2==0){
+        sumnumbers += i; 
+    }
+}
+console.log(sumnumbers)
+
+
+// davaleba 11
+// write a program that prints the sum of all numbers between 1 and 1000 that are divisible by 3 or 5.
+
+let sumnumbers2 = 0;
+for (let i = 1; i <= 1000; i++) { 
+    if(i%3==0 || i%5==0){
+        sumnumbers2 += i; 
+    }
+}
+console.log(sumnumbers2)
+
+
+// davaleba 12
+// write a program that prints the sum of all numbers between 1 and 1000 that are divisible by both 3 and 5.
+
+let sumnumbers3 = 0;
+for (let i = 1; i <= 1000; i++) { 
+    if(i%3==0 && i%5==0){
+        sumnumbers3 += i; 
+    }
+}
+console.log(sumnumbers3)
