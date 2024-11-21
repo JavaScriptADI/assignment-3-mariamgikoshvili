d919e932b7eb343f7620b602c47e6e17d12c3e6f
mariamgikoshvili <mgiko17@freeuni.edu.ge>
2024-11-21 16:45:31 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,93 @@
 // write your code in this directory
+
+// [Challenge] "Mystery Room", Write a code that will generate 2d representation of a room. you should generate an empty space, wall or a mystery box. You can use any characters you want. The room should be 10x10. The room should have 10 walls, 1 mystery boxes and the rest should be empty space, mystey box location should be random!. The room should look something like this:
+
+const width¬
+
+for(let row=0;row<)
+
+// davaleba 1
+
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+// davaleba 2
+
+for(let i = 10; i >= 1; i--){
+    console.log(i)
+}
+
+// davaleba 3
+let sum = 0; /*გამოცხადდა ცვლადი*/
+for (let i = 1; i <= 50; i++) { /*გავივლით რიცხვებს 1 დან 50ის ჩათვლით*/
+    sum += i; /*მივუმატებთ გამოცხადებულ ცვლადს*/
+}
+console.log(sum); /*დაიბეჭდება საბოლოო ჯამი */
+
+// davaleba 4
+for (let i = 0; i < 10; i += 2) {
+    console.log(i);
+}
+
+// 5 იტერაციაა - დაიბეჭდება 0, 2, 4, 6, 8
+
+// davaleba 5
+// What does the break statement do? Explain and give an example.
+
+// შეწყვეტს ციკლს
+for (let i = 0; i < 10; i += 2) {
+    if(i==6) break
+    console.log(i);  
+}
+
+// davaleba 6
+// What does the break statement do? Explain and give an example.
+
+// შეწყვეტს ციკლს
+for (let i = 0; i < 10; i += 2) {
+    if(i==6) continue
+    console.log(i);  
+}
+
+// აქ გამოტოვებს 6 ს
+
+
+// davaleba 7
+let i ;
+
+while (true){
+    i = Math.random() * 100
+    console.log(i)
+    if (i == 27) break
+}
+
+// davaleba 8
+
+for(let i = 1; i<=100; i++){
+    if(i%15==0){
+        console.log('FizzBuzz')
+    }
+    else if(i%3==0){
+        console.log('Fizz')
+    }
+    else if(i%5==0){
+        console.log('buzz')
+    }
+    else {
+       console.log(i)
+    }
+}
+
+
+
+// davaleba 9
+
+for(let i = 0; i<100; i++){
+    console.log(Math.floor(math.random() * 101)+1)
+}
+
+// davaleba 10
+
+// write a program that prints the sum of all even numbers between 1 and 1000.
+
